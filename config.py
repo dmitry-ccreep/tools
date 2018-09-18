ac3038f06513989a387734790a917dd2b7ea2b83
CORP\Dmitry.Romanovskiy <dmitry.romanovskiy@acronis.com>
2018-09-19 00:46:24 +0300

    Updated PATH

--- config.py
+++ config.py
@@ -1,1 +1,2 @@
-DRIVER_PATH = r'C:\Users\Dmitry\Documents\ccreep_tools\chrome'
+# DRIVER_PATH = r'C:\Users\Dmitry\Documents\ccreep_tools\chrome'
+DRIVER_PATH = r'C:\Users\dmitry.romanovskiy\Documents\ccreep_tools\chrome'
